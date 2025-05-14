30df256e610824cd2639036e7fd8b41dd5fcc2d5
HardeepIjardar <devijardar2@gmail.com>
2025-05-14 21:29:39 +0530

    Icon fixed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logoImage from '../assets/images/logo.png';
 
 const Logo: React.FC<{ size?: number }> = ({ size = 40 }) => {
   return (
     <img 
-      src={logoImage} 
+      src={`${process.env.PUBLIC_URL}/favicon.ico`} 
       alt="ArtiFlare Logo" 
       style={{ width: size, height: size, objectFit: 'contain' }}
     />
